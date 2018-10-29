00a354de69e654851e38173aec1b6839bc06e29a
Albert <junyuo@gmail.com>
2018-10-29 14:59:02 +0800

    移除 page1

--- demo1/src/app/app.module.ts
+++ demo1/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { Page1Component } from './page1/page1.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Page1Component
+    AppComponent
   ],
   imports: [
     BrowserModule
