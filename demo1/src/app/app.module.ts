57d6441c7bbde9869ff21b8efdd626045cad91b2
Albert <junyuo@gmail.com>
2018-10-29 15:09:39 +0800

    透過 ng g c page2 建立 page2 component

--- demo1/src/app/app.module.ts
+++ demo1/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Page2Component } from './page2/page2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page2Component
   ],
   imports: [
     BrowserModule
