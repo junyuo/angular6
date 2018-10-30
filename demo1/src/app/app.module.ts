20fca81eafc50e7f62c75c4c99b6b7ccc8db9541
Albert <junyuo@gmail.com>
2018-10-30 11:21:28 +0800

    component directive

--- demo1/src/app/app.module.ts
+++ demo1/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { Page2Component } from './page2/page2.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Page2Component,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
