f16cd28ec4f99caf00412056995b3ddf727bfbf5
Albert <junyuo@gmail.com>
2018-11-01 11:37:02 +0800

    async example

--- demo1/src/app/article/article-list/article-list.component.ts
+++ demo1/src/app/article/article-list/article-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -10,38 +11,13 @@ import { DataService } from '../data.service';
 export class ArticleListComponent implements OnInit {
 
   isEdited = false;
-  data;
+  data$: Observable<any>;
 
   constructor(public dataSvc: DataService) {
   }
 
   ngOnInit() {
-    this.dataSvc.getData().subscribe(result => {
-      this.data = result;
-    });
-  }
-
-  public doDelete(item) {
-    this.dataSvc.doDelete(item).subscribe(result => {
-      this.data =
-        this.data.filter((v) => {
-          return v.id !== item.id;
-        });
-    },
-      (error) => {
-        console.log(error);
-      });
-  }
-
-  public doModify(post: any) {
-    this.dataSvc.doModify(post).subscribe(result => {
-      this.data = this.data.map((item) => {
-        if (item.id === post.id) {
-          return Object.assign({}, item, post);
-        }
-        return item;
-      }, (error) => console.log(error));
-    });
+    this.data$ = this.dataSvc.getData();
   }
 
 }
